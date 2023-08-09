dd9d9b58982f404efc9e90969c36a60824cc0c13
Nikan <nikan.doosti@outlook.com>
2023-08-09 10:36:32 +0330

    add test packages to dependency (optional)
    You can install via `pip install .[test]

--- setup.py
+++ setup.py
@@ -3,6 +3,16 @@ from setuptools import setup
 with open('VERSION', 'r') as file:
     version = file.read().strip()
 
+test_deps = [
+    'httpx>=0.24',
+    'pytest>=7.3.0',
+]
+extras = {
+    'test': test_deps,
+}
+
 setup(
-    version=version,
+    tests_require=test_deps,
+    extras_require=extras,
+    version=version
 )
