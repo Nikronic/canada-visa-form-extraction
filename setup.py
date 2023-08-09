5180d611d6b40f6789b647d960d7668be9f230b0
Nikronic <nikan.doosti@outlook.com>
2023-08-09 16:40:46 +0330

    now only `pyproject.toml` contains setup configs (removed `cfg` and `py`

--- setup.py
+++ setup.py
@@ -1,18 +1,3 @@
 from setuptools import setup
 
-with open('VERSION', 'r') as file:
-    version = file.read().strip()
-
-test_deps = [
-    'httpx>=0.24.1',
-    'pytest>=7.4.0',
-]
-extras = {
-    'test': test_deps,
-}
-
-setup(
-    tests_require=test_deps,
-    extras_require=extras,
-    version=version
-)
+setup()
