292f0b177f98358e71724157c9fbf2189427f753
Nikan Doosti <nikan.doosti@outlook.com>
2024-01-08 15:31:56 +0330

    replace relative import with abs (package)
    in `src`-layout, you must import as it is package is 3rdparty and should ignore relative imports inside sub-packages

--- src/cvfe/__init__.py
+++ src/cvfe/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ["BASE_SOURCE_DIR", "adobe_xfa_processor"]
+__all__ = ["adobe_xfa_processor"]
 
 import logging
 
@@ -6,5 +6,4 @@ logger = logging.getLogger(__name__)
 
 
 # main functions, class, constants, etc that end-user wants
-from api.convert import BASE_SOURCE_DIR
-from api.convert.adobe_xfa import process as adobe_xfa_processor
+from cvfe.api.convert.adobe_xfa import process as adobe_xfa_processor
